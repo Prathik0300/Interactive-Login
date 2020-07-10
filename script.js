5ae4a10fea55fca5f12dae348a6db760766d4564
Prathik Pugazhenthi <prathik0300@gmail.com>
2020-07-10 13:33:32 +0530

    project done

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function openEye(){
+    var open = document.getElementById("open");
+    var close = document.getElementById("close");
+    open.style.display = "";
+    close.style.display = "none";
+    
+}
+
+
+function closeEye(){
+    var open = document.getElementById("open");
+    var close = document.getElementById("close");
+    open.style.display = "none";
+    close.style.display = "";
+}
